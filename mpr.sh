4c56c54bf3cf780f3779064d3e677a74c0fbbb20
Adam Cvikl <adam.wasd4@gmail.com>
2022-07-06 13:11:05 +0200

    v1.1
    - changed from sh to bash
    - script now checks if package exists, so it won't make mess on your home folder
    - now using printf instead of echo

--- mpr.sh
+++ mpr.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 read -p "Enter package name: " pkgname
-echo Downloading: $pkgname\n
-git clone "https://mpr.makedeb.org/$pkgname.git"
-cd $pkgname/
-makedeb -s -i
+printf "Checking if package exists...\n"
+pkgexistence=$(git ls-remote --exit-code https://mpr.makedeb.org/$pkgname.git)
+if [[ $pkgexistence > 0 ]]
+then
+    printf "Package exists!\n"
+    printf "Downloading: $pkgname\n"
+    git clone "https://mpr.makedeb.org/$pkgname.git"
+    cd $pkgname/
+    makedeb -s -i
+else
+    printf "Package doesn't exist!\n"
+fi
